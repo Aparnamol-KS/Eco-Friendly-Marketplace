dff115a66d1231f779e0679a3d74c3fc9d728660
Aparnamol K S <aparnamolks.off@gmail.com>
2024-10-12 22:29:45 +0530

    Updating register feature

--- Eco_products/Eco_webapp/admin.py
+++ Eco_products/Eco_webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserDetails
 # Register your models here.
+ 
+
+# Register the model with the Django admin site
+admin.site.register(UserDetails)
+
--- Eco_products/Eco_webapp/models.py
+++ Eco_products/Eco_webapp/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+class UserDetails(models.Model):
+    user_id = models.AutoField(primary_key=True)  # Auto-incrementing ID
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  # Password can be stored as hashed
+    role = models.CharField(
+        max_length=10, 
+        choices=(('Buyer', 'Buyer'), ('Admin', 'Admin'))
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
 
